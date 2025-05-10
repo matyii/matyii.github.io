59df1f1e514b56a20bb9efcf5301fc77410d2a81
matyii <kmatyas2004@gmail.com>
2025-05-11 00:48:11 +0200

    projects page

---
+++ src/lib/components/custom/Navbar.svelte
@@ -0,0 +1,41 @@
+<script lang='ts'>
+    import * as Avatar from "$lib/components/ui/avatar/index.js";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import ThemeToggler from "$lib/components/custom/ThemeToggler.svelte";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { Separator } from "$lib/components/ui/separator/index.js";
+</script>
+
+<nav class="flex items-center justify-between p-2 m-8">
+  <div class="flex items-center space-x-4">
+    <Avatar.Root class="size-10">
+        <Avatar.Image src="https://github.com/matyii.png" alt="@matyii" />
+        <Avatar.Fallback>MK</Avatar.Fallback>
+    </Avatar.Root>
+  </div>
+
+  <div class="hidden md:flex space-x-4">
+    <Button variant="ghost" href="/">Home</Button>
+    <Button variant="ghost" href="/projects">My projects</Button>
+    <ThemeToggler />
+  </div>
+
+  <div class="md:hidden">
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <Button variant="ghost">Menu</Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content class="rounded shadow-md">
+        <DropdownMenu.Group>
+          <DropdownMenu.Item>Home</DropdownMenu.Item>
+        </DropdownMenu.Group>
+        <DropdownMenu.Group class='flex justify-center py-2'>
+          <ThemeToggler />
+        </DropdownMenu.Group>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </div>
+</nav>
+<div class="flex justify-center mx-8">
+  <Separator />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,45 @@
 <script lang="ts">
     import * as Avatar from "$lib/components/ui/avatar/index.js";
     import * as Card from "$lib/components/ui/card/index.js";
-    import ThemeToggler from "$lib/components/custom/ThemeToggler.svelte";
+    import Navbar from "$lib/components/custom/Navbar.svelte";
+    import Button from "$lib/components/ui/button/button.svelte";
+    import * as HoverCard from "$lib/components/ui/hover-card/index.js";
 </script>
-
+<Navbar />
 <div class="flex items-center justify-center min-h-screen">
   <Card.Root class="w-full max-w-sm">
-    <Card.Header class="flex flex-col items-center justify-center space-y-2">
+    <Card.Header class="flex flex-col items-center justify-center space-y-2 text-center">
       <Avatar.Root>
         <Avatar.Image src="https://github.com/matyii.png" alt="@matyii" />
         <Avatar.Fallback>MK</Avatar.Fallback>
       </Avatar.Root>
-      <Card.Title>@matyii</Card.Title>
-      <Card.Description>Developer</Card.Description>
-    </Card.Header>
+      <Card.Title>hi there.</Card.Title>
+      <Card.Description>i'm 
+      <HoverCard.Root>
+      <HoverCard.Trigger
+        href="https://github.com/sveltejs"
+        target="_blank"
+        rel="noreferrer noopener"
+        class="rounded-sm underline-offset-4 hover:underline focus-visible:outline-2 focus-visible:outline-offset-8 focus-visible:outline-black">matyii
+      </HoverCard.Trigger>
+      <HoverCard.Content class="">
+        <div class="flex justify-between space-x-4">
+          <Avatar.Root>
+            <Avatar.Image src="https://github.com/matyii.png" />
+            <Avatar.Fallback>MK</Avatar.Fallback>
+          </Avatar.Root>
+          <div class="space-y-1">
+            <h4 class="text-sm font-semibold">@matyii</h4>
+            <p class="text-sm text-muted-foreground">20 year old it technician</p>
+          </div>
+        </div>
+      </HoverCard.Content>
+    </HoverCard.Root>  
+    , a 20-year old IT technician, hobbyist programmer, server administrator</Card.Description>
+  </Card.Header>
 
-    <Card.Content class="flex justify-center">
-      <ThemeToggler />
+    <Card.Content class="flex justify-center gap-2">
+      <!-- <Button href="https://github.com/matyii" target="_blank">my github</Button> -->
     </Card.Content>
   </Card.Root>
 </div>
