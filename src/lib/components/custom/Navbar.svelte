efbbd4db68202540392b74bf0062060e498124fa
matyii <kmatyas2004@gmail.com>
2025-05-16 00:56:04 +0200

    updated aria labels and static content

--- src/lib/components/custom/Navbar.svelte
+++ src/lib/components/custom/Navbar.svelte
@@ -9,7 +9,7 @@
 <nav class="flex items-center justify-between p-2 m-6">
   <div class="flex items-center space-x-4">
     <Avatar.Root class="size-10">
-        <Avatar.Image src="https://github.com/matyii.png" alt="@matyii" />
+        <Avatar.Image src="/favicon.png" alt="@matyii" />
         <Avatar.Fallback>MK</Avatar.Fallback>
     </Avatar.Root>
   </div>
